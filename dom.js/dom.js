c35736f0314368d16d50050ab01eb225266070d8
Aman Gupta <ag9797191@gmail.com>
2024-06-13 13:26:55 +0530

    add

---
+++ dom.js/dom.js
@@ -0,0 +1,21 @@
+// document.body.style.background="blue";
+
+// for(i=0; i<document.body.childNodes.length; i++){
+//     alert(document.body.childNodes[i]);
+// }
+// for(let node of document.body.childNodes){
+//     alert(node)
+// }
+// alert(document.body.childNodes(i));
+
+// for(let elem of document.body.children){
+//     alert(elem)
+// }
+document.body.style.background="cyan"
+// setTimeout(()=>document.body.style.background="",10000)
+// alert( "From HEAD: " + document.body ); 
+// for(let i=0; i<document.body.childNodes.length; i++){
+//     alert(document.body.childNodes[i]);
+// }
+
+document.body.style.color='grey';   
