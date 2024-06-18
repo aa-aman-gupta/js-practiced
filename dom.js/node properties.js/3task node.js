b75f439d136466c27b18c93c3a0ff0978858ec15
Aman Gupta <ag9797191@gmail.com>
2024-06-18 12:53:42 +0530

    add

---
+++ dom.js/node properties.js/3task node.js
@@ -0,0 +1,17 @@
+/* What does this code show?
+
+<script>
+  let body = document.body;
+
+  body.innerHTML = "<!--" + body.tagName + "-->";
+
+  alert( body.firstChild.data ); // what's here?
+</script> */
+
+// let myBody=Document.body;
+// myBody.innerHTML=' <    '
+
+let str="hello";
+console.log(typeof str);
+let a=str+1;
+console.log(typeof a)
---
+++ dom.js/node properties.js/practice.js
@@ -0,0 +1,17 @@
+// alert(document.body instanceof Text);
+// alert("hello")
+// console.log(Element)
+// let elem=document.body
+// alert(document.body.firstChild.tagName)   ;
+// alert(document.body.innerHTML);
+
+
+// document.body.innerHTML='the new body is here '
+// alert(document.body.innerHTML='<h1>hello</h1>')
+
+// document.body.innerHTML+="hello brother";
+// let amn=document.getElementById("os");
+// alert(amn.innerHTML)
+
+// let text = document.body.firstChild;
+// alert(text.data)
